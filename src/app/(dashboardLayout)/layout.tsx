f5b69aa59402ec8e7cf5f646699e0b91000626a9
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-12-04 20:33:24 +0600

    Setting Up The Dashboard Layout Structure

--- src/app/(dashboardLayout)/layout.tsx
+++ src/app/(dashboardLayout)/layout.tsx
@@ -1,5 +1,5 @@
-import LogoutButton from "@/components/shared/LogoutButton";
-import { getCookie } from "@/services/auth/tokenHandlers";
+import DashboardNavbar from "@/components/modules/Dashboard/DashboardNavbar";
+import DashboardSidebar from "@/components/modules/Dashboard/DashboardSidebar";
 import React from "react";
 
 export const dynamic = "force-dynamic";
@@ -9,13 +9,13 @@ const CommonDashboardLayout = async ({
 }: {
   children: React.ReactNode;
 }) => {
-  const accessToken = await getCookie("accessToken");
   return (
     <div className="flex h-screen overflow-hidden">
+      <DashboardSidebar />
       <div className="flex flex-1 flex-col overflow-hidden">
+        <DashboardNavbar />
         <main className="flex-1 overflow-y-auto bg-muted/10 p-4 md:p-6">
-          {accessToken && <LogoutButton />}
-          <div className="">{children}</div>
+          <div className="max-w-7xl">{children}</div>
         </main>
       </div>
     </div>
